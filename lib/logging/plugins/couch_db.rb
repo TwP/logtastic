7b13961766f3709d324884c54b1c2dbaae523a9d
Tim Pease <tim.pease@gmail.com>
2009-07-22 11:15:03 -0600

    initial commit

---
+++ lib/logging/plugins/couch_db.rb
@@ -0,0 +1,14 @@
+
+root = File.expand_path(
+  File.join(File.dirname(__FILE__), '..'))
+
+module Logging::Plugins
+  module CouchDB
+    def initialize_couch_db
+    end
+  end  # module CouchDB
+end  # module Logging::Plugins
+
+require File.join(root, 'couch_db')
+
+# EOF
