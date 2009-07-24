448ec4de6a0721794fe71faca672d268dca7362d
Tim Pease <tim.pease@gmail.com>
2009-07-24 14:27:52 -0600

    adding a live tail page so you can watch log messages as they are inserted into the couch DB instance

--- logging_couchapp/_attachments/logging.js
+++ logging_couchapp/_attachments/logging.js
@@ -6,7 +6,7 @@ function Logging(app) {
     if (num >= 0 && num < this.levels.length) {
       return this.levels[num];
     }
-    return null;
+    return 'unknown';
   };
 
   this.selector = function( str ) {
@@ -20,6 +20,26 @@ function Logging(app) {
     }
     str += '</tr>';
     return str;
+  };
+
+  this.log_event_row = function( doc ) {
+    var timestamp = this.format_timestamp(doc);
+    return '<tr id="'+doc._id+'" class="color'+doc.level+'">'
+      + '<td data-timestamp="'+timestamp+'">'+app.prettyDate(timestamp)+'</td>'
+      + '<td>'+doc.app_id+'</td>'
+      + '<td>'+doc.logger+'</td>'
+      + '<td>'+this.level_name(doc.level).capitalize()+'</td>'
+      + '<td>'+doc.message+'</td>'
+      + '</tr>';
+  };
+
+  this.format_timestamp = function( obj ) {
+    var timestamp = obj.timestamp;
+    if (timestamp == undefined) { timestamp = obj; }
+    return timestamp.
+        replace(/-/g,'/').
+        replace(/T/, ' ').
+        replace(/\.\d+Z$/, ' UTC');
   };
 
 };
